42b4c10cfe0d7f6fb416768c84442f4ec2c0721a
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-07-25 08:25:52 +0100

    .

--- personal-portfolio/src/components/VerticalSlider/VerticalSlider.tsx
+++ personal-portfolio/src/components/VerticalSlider/VerticalSlider.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import './slider.css'
 
 function VerticalSlider() {
@@ -15,26 +16,59 @@ function VerticalSlider() {
     }
 
     return (
-        <div className="vertical-slider d-flex align-items-center justify-content-center h-100 m-5">
-    <input
-        type="range"
-        className="form-range custom-vertical-slider"
-        style={{ writingMode: 'vertical-rl', height: '400px', width: '50px', borderRadius: '50px', outline: 'none' }}
-        list="tickmarks"
-        step="1"
-        value={value}
-        onChange={handleChange}
-        onMouseDown={handleMouseDown}
-    />
-    <datalist id="tickmarks">
-        <option value="0" label="0%" />
-        <option value="25" label="25%" />
-        <option value="50" label="50%" />
-        <option value="75" label="75%" />
-        <option value="100" label="100%" />
-    </datalist>
-</div>
-
+        <Container fluid className="h-100">
+            <Row className="h-100">
+                <Col className="d-flex align-items-center justify-content-around float-end text-center flex-column" xs={8}>
+                    <Row className="mb-3">
+                        <Col xs={12} className="text-center rounded-5 d-flex align-items-center justify-content-center px-5 bubble">
+                            <p className="text-white">TEST</p>    
+                        </Col>  
+                    </Row>
+                    <Row className="mb-3">
+                        <Col xs={12} className="text-center. rounded-5 d-flex align-items-center justify-content-center px-5 bubble">
+                            <p className="text-white">efefef</p>    
+                        </Col>  
+                    </Row>
+                    <Row className="mb-3">
+                        <Col xs={12} className="text-center rounded-5 d-flex align-items-center justify-content-center px-5 bubble">
+                            <p className="text-white">fdeffefef.</p>    
+                        </Col>  
+                    </Row>
+                    <Row>
+                        <Col xs={12} className="text-center rounded-5 d-flex align-items-center justify-content-center px-5 bubble">
+                            <p className="text-white">dfvfdvd</p>    
+                        </Col>  
+                    </Row>
+                </Col>
+                <Col className="d-flex align-items-center justify-content-center" xs={4}>
+                    <div className="vertical-slider d-flex align-items-center justify-content-center h-100 mb-3">
+                        <input
+                            type="range"
+                            className="form-range custom-vertical-slider"
+                            style={{
+                                writingMode: 'vertical-rl',
+                                height: '400px',
+                                width: '50px',
+                                borderRadius: '50px',
+                                outline: 'none'
+                            }}
+                            list="tickmarks"
+                            step="1"
+                            value={value}
+                            onChange={handleChange}
+                            onMouseDown={handleMouseDown}
+                        />
+                        <datalist id="tickmarks">
+                            <option value="0" label="0%" />
+                            <option value="25" label="25%" />
+                            <option value="50" label="50%" />
+                            <option value="75" label="75%" />
+                            <option value="100" label="100%" />
+                        </datalist>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
