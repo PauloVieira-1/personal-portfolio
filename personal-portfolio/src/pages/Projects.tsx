21001b2f325ebfb57f4ba908d5d80b1547e6da2c
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-07-30 16:49:10 +0100

    .

--- personal-portfolio/src/pages/Projects.tsx
+++ personal-portfolio/src/pages/Projects.tsx
@@ -1,14 +1,10 @@
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 import cardData from "./ProjectsContent";
+import { useState } from "react";
+import { customColors } from "../types";
 
-
-function Projects() {
-  return (
-    <div className="text-white text-start">
-            <h1 className="display-3 fw-bold mb-2 bottom-fade-in-fast">Projects</h1>
-      <h2 className="fs-3 text-danger mb-4 bottom-fade-in-fast">My Work</h2>
-
-      {cardData.map((card) => <ProjectCard
+const tabs = {
+  1: cardData.map((card) => <ProjectCard
         title={card.title}
         description={card.description}
         images={card.images}
@@ -18,8 +14,34 @@ function Projects() {
         liveLink={card.liveLink}
         key={card.title}
         techStack={card.techStack}
-      />
-      )}
+      />),
+  2: <p>Coming soon...</p>,
+};
+
+
+function Projects() {
+  const [currentTab, setCurrentTab] = useState(1);
+
+  const changeTab = (tab: number) => {
+    setCurrentTab(tab);
+    console.log("TEST")
+  };
+
+  return (
+    <div className="text-white text-start">
+      <h1 className="display-3 fw-bold mb-2 bottom-fade-in-fast">Projects</h1>
+      <h2 className="fs-3 text-danger mb-4 bottom-fade-in-fast">My Work</h2>
+      <div className="d-flex justify-content-center">
+        <button
+          className={`shadow-butto tab-button ${currentTab === 1 ? "active-tab" : ""}`}
+          onClick={() => changeTab(1)}
+        > Web Projects </button>
+        <button
+          className={`shadow-butto tab-button ${currentTab === 2 ? "active-tab" : ""}`}
+          onClick={() => changeTab(2)}
+        > Other </button>
+      </div>
+      <div className="tab-content">{tabs[currentTab]}</div>
     </div>
   );
 }
